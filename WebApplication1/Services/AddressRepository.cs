7579c82c2f70e326c742dc7a6ef816c4ab05d5c8
Mateusz <mateusz.wolnica@gmail.com>
2025-05-31 22:03:15 +0200

    .

---
+++ WebApplication1/Services/AddressRepository.cs
@@ -0,0 +1,86 @@
+﻿using NHibernate;
+using WebApplication1.Domain;
+using ISession = NHibernate.ISession;
+
+namespace NHibernateTestBlog
+{
+    public interface IAddressRepository
+    {
+        Task<int> Add(Address product);
+        Task Update(Address product);
+        Task Remove(Address product);
+        Task<Address> GetById(int addressId);
+        Task<Address> GetByName(string name);
+    }
+
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly INHibernateHelper _nHibernateHelper;
+        public AddressRepository(INHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public async Task<int> Add(Address product)
+        {
+            using (ISession session = _nHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                var result = await session.SaveAsync(product);
+                await transaction.CommitAsync();
+
+                return (int)result;
+            }
+        }
+        
+        public async Task Update(Address product) 
+        {
+            using (ISession session = _nHibernateHelper.OpenSession()) 
+            using (ITransaction transaction = session.BeginTransaction()) 
+            { 
+                await session.UpdateAsync(product); 
+                await transaction.CommitAsync(); 
+            }
+        }
+
+        public async Task Remove(Address product) 
+        {
+            using (ISession session = _nHibernateHelper.OpenSession()) 
+            using (ITransaction transaction = session.BeginTransaction()) 
+            { 
+                await session.DeleteAsync(product); 
+                await transaction.CommitAsync(); 
+            }
+        } 
+        
+        public async Task<Address> GetById(int productId) 
+        {
+            using (ISession session = _nHibernateHelper.OpenSession())
+            {
+                return await session.GetAsync<Address>(productId);
+            }
+        }
+
+        public async Task<Address> GetByName(string name)
+        {
+            using (ISession session = _nHibernateHelper.OpenSession())
+            {
+                // Corrected to use QueryOver for LINQ-like querying
+                Address product = await session.QueryOver<Address>()
+                    .Where(p => p.Country == name)
+                    .SingleOrDefaultAsync();
+
+                return product;
+            }
+        }
+
+    //    var subquery = QueryOver.Of<AddressCompany>()
+    //.Where(c => c.CreationDate > new DateTime(2000, 1, 1))
+    //.Select(c => c.Company.ID);
+
+    //    var query = session.QueryOver<Company>()
+    //        .WithSubquery
+    //        .WhereProperty(c => c.ID)
+    //        .In(subquery)
+    }
+}
---
+++ WebApplication1/Services/CompanyRepository.cs
@@ -0,0 +1,85 @@
+﻿using NHibernate;
+using NHibernate.Criterion;
+using WebApplication1.Domain;
+using ISession = NHibernate.ISession;
+
+namespace NHibernateTestBlog
+{
+    public interface ICompanyRepository
+    {
+        Task<int> Add(Company product);
+        Task Update(Company product);
+        Task Remove(Company product);
+        Task<Company> GetById(int addressId);
+        Task<Company> GetBySubquery();
+    }
+
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly INHibernateHelper _nHibernateHelper;
+        public CompanyRepository(INHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        public async Task<int> Add(Company product)
+        {
+            using (ISession session = _nHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                var result = await session.SaveAsync(product);
+                await transaction.CommitAsync();
+
+                return (int)result;
+            }
+        }
+        
+        public async Task Update(Company product) 
+        {
+            using (ISession session = _nHibernateHelper.OpenSession()) 
+            using (ITransaction transaction = session.BeginTransaction()) 
+            { 
+                await session.UpdateAsync(product); 
+                await transaction.CommitAsync(); 
+            }
+        }
+
+        public async Task Remove(Company product) 
+        {
+            using (ISession session = _nHibernateHelper.OpenSession()) 
+            using (ITransaction transaction = session.BeginTransaction()) 
+            { 
+                await session.DeleteAsync(product); 
+                await transaction.CommitAsync(); 
+            }
+        } 
+        
+        public async Task<Company> GetById(int productId) 
+        {
+            using (ISession session = _nHibernateHelper.OpenSession())
+            {
+                return await session.GetAsync<Company>(productId);
+            }
+        }
+        public async Task<Company> GetBySubquery()
+        {
+            using (ISession session = _nHibernateHelper.OpenSession())
+            {
+                var subquery = QueryOver.Of<AddressCompany>()
+                    .Where(c => c.CreationDate > new DateTime(2000, 1, 1))
+                    .Select(c => c.Company.Id);
+
+                var query = await session.QueryOver<Company>()
+                    .WithSubquery
+                    .WhereProperty(c => c.Id)
+                    .In(subquery)
+                    .SingleOrDefaultAsync();
+
+                return query;
+            }
+        }
+
+
+        
+    }
+}
