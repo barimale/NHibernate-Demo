554309346e30a0d7613bd65625c51013274a093b
Mateusz <mateusz.wolnica@gmail.com>
2025-06-12 18:56:09 +0200

    .

--- WebApplication1/DTOs/AddressDto.cs
+++ WebApplication1/DTOs/AddressDto.cs
@@ -10,6 +10,5 @@ namespace Demo.API.DTOs
         public virtual string ZipCode { get; set; }
         public virtual string Country { get; set; }
         public virtual string Phone { get; set; }
-        public virtual IList<CompanyDto> Companies { get; set; } = new List<CompanyDto>();
     }
 }
