daa08fd87cb987e8c81ba515f453cbdbc8e40ad6
Mateusz <mateusz.wolnica@gmail.com>
2025-06-14 17:26:42 +0200

    .

--- Migrations/Migrations/20250605193247_AddCompanyAddress.cs
+++ Migrations/Migrations/20250605193247_AddCompanyAddress.cs
@@ -30,7 +30,6 @@ namespace Demo.Migrations.Migrations
             Create.ForeignKey("FK_CompanyAddress_Company")
                 .FromTable(TableName).ForeignColumn("CompanyId")
                 .ToTable(CompanyTable).PrimaryColumn("Id");
-
         }
 
         public override void Down()
