4f833b3baaaadb9d32b0eb11620be266db558cdc
Mateusz <mateusz.wolnica@gmail.com>
2025-06-14 22:49:42 +0200

    .

--- WebApplication1/DTOs/ProductDto.cs
+++ WebApplication1/DTOs/ProductDto.cs
@@ -3,21 +3,62 @@ using FluentValidation;
 
 namespace Demo.API.DTOs
 {
+    /// <summary>
+    /// Data Transfer Object for representing a product.
+    /// Inherits common properties from <see cref="BaseDto"/>.
+    /// </summary>
     public class ProductDto : BaseDto
     {
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public ProductTypeDto Type { get; set; }
-        public bool Discontinued { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the product.
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the category of the product.
+        /// </summary>
+        public required string Category { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the product.
+        /// </summary>
+        public required ProductTypeDto Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the product is discontinued.
+        /// </summary>
+        public required bool Discontinued { get; set; }
     }
 
+    /// <summary>
+    /// Validator for <see cref="ProductDto"/> using FluentValidation.
+    /// </summary>
     public class ProductValidator : AbstractValidator<ProductDto>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductValidator"/> class.
+        /// Defines validation rules for <see cref="ProductDto"/> properties.
+        /// </summary>
         public ProductValidator()
         {
+            // Name is required and must be between 2 and 50 characters.
+            RuleFor(user => user.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
+
+            // Category is required and must be between 2 and 50 characters.
             RuleFor(user => user.Category)
                 .NotEmpty().WithMessage("Category is required.")
                 .Length(2, 50).WithMessage("Category must be between 2 and 50 characters.");
+
+            // Type must not be null.
+            RuleFor<ProductTypeDto>(user => user.Type)
+                .NotNull().WithMessage("Type is required.");
+
+            // Discontinued must be specified as a boolean value.
+            RuleFor(user => user.Discontinued)
+                .NotNull().WithMessage("Discontinued status is required.")
+                .Must(x => x == true || x == false).WithMessage("Discontinued must be a boolean value.");
         }
     }
 }
